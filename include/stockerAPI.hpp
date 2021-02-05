a9695e476ffa18eab43d61c2f32aee4e299dca79
DavidM-Fox <david.mac.fox@gmail.com>
2021-02-05 15:58:35 -0600

    Libcurl functionality implemented

--- include/stockerAPI.hpp
+++ include/stockerAPI.hpp
@@ -11,130 +11,156 @@
 
 namespace stocker
 {
-    //Array of date pairs [YYYY-MM-DD, Unix Timestamp]
-    typedef std::vector<std::vector<std::string>> period;
 
     class quote
     {
         public:
-            quote();
-            void set_interval(std::string t_interval);
-            void set_period(std::string t_period_start, std::string t_period_end);
-            void set_symbol(std::string t_symbol);
-            
-            stocker::period get_period();
+            quote
+            (
+                std::string t_symbol     = "---", 
+                std::string t_date_start = "YYYY-MM-DD", 
+                std::string t_date_end   = "YYYY-MM-DD", 
+                std::string t_interval   = "1wk"
+            );
 
-            void print_period();
+            void set_symbol(std::string t_symbol);
+            void set_period(std::string t_date_start, std::string t_date_end);
+            void set_interval(std::string t_interval);
+            
+            void download_quote_data(std::string filename);
+            
+            void print_quote_info();
 
             ~quote();
     
         private:
-            /* data */
-            std::string m_url;
+            std::string unix_timestamp(std::string t_date);
+            bool string_replace(std::string& str, const std::string from, const std::string to);
+            void build_url();
+            
             std::string m_symbol;
-            std::vector<std::string> m_date_start; 
-            std::vector<std::string> m_date_end; 
+            std::string m_date_start; 
+            std::string m_date_end; 
             std::string m_interval;
+            std::string m_url;
     };
+    
 
-    /*
-    * quote constructor: m_url, m_sybol, m_date_start, m_date_end, m_interval
-    * set to default
-    */
-    quote::quote(/* args */) :
-    m_url("https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={period_start}&period2={period_end}&interval={interval}&events=history"),
-    m_symbol("---"),
-    m_date_start(2, ""),
-    m_date_end(2, ""),
-    m_interval("1wk") { }
-
-    /*
-    * Sets the interval of historical stock data: ["1d", "1wk", "1mo"]
-    */
-    void quote::set_interval(std::string t_interval)
+    // quote constructor: m_symbol, m_date_start, m_date_end, m_interval, m_url set to default
+    quote::quote(std::string t_symbol, std::string t_date_start, std::string t_date_end, std::string t_interval) :
+    m_symbol(t_symbol),
+    m_date_start(t_date_start),
+    m_date_end(t_date_end),
+    m_interval(t_interval),
+    m_url("https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={date_start}&period2={date_end}&interval={interval}&events=history")
     {
-        this->m_interval = t_interval;
+        build_url();
     }
     
-    /*
-    * Takes an end and start date in the format YYYY-MM-DD, converting both strings 
-    * to unix timestamps. Sets m_date_start & m_date_end vectors to 
-    * ["YYYY-MM-DD", "Unix Timestamp"]
-    */
-    void quote::set_period(std::string t_date_start, std::string t_date_end)
-    {
-
-        struct std::tm start_time = {0,0,0,0,0,0,0,0,0};
-        struct std::tm end_time   = {0,0,0,0,0,0,0,0,0};
-        std::time_t epoch;
-
-        std::istringstream iss(t_date_start + ' ' + t_date_end);
-        
-        iss >> std::get_time(&start_time, "%Y-%m-%d"); 
-        if(iss.fail())
-        {
-            std::cerr  << "ERROR: Cannot parse starting date (" << t_date_start <<"); required format YYYY-MM-DD" << std::endl;
-            exit(1);
-        }
-        
-        iss >> std::get_time(&end_time, "%Y-%m-%d");
-        if(iss.fail())
-        {
-            std::cerr  << "ERROR: Cannot parse ending date (" << t_date_end <<"); required format YYYY-MM-DD" << std::endl;
-            exit(1);
-        }
-        
-        start_time.tm_hour = 0;
-        start_time.tm_min  = 0;
-        start_time.tm_sec  = 0;
-        epoch = std::mktime(&start_time);
-        this->m_date_start[0] = t_date_start;
-        this->m_date_start[1] = std::to_string(epoch);
-        
-        end_time.tm_hour = 0;
-        end_time.tm_min  = 0;
-        end_time.tm_sec  = 0;
-        epoch = std::mktime(&end_time);
-        this->m_date_end[0] = t_date_end;
-        this->m_date_end[1] = std::to_string(epoch);
-    }
-
-    /*
-    * Returns the period of the quote:
-    * std::vector<std::vector<std::string>>
-    * 
-    * [
-    *   ["YYYY-MM-DD", UNIX_TIMESTAMP], //date start
-    *   ["YYYY-MM-DD", UNIX_TIMESTAMP]  //date end
-    * ]
-    * 
-    */
-    stocker::period quote::get_period()
-    {
-        stocker::period this_period{this->m_date_start, this->m_date_end};
-        return this_period;
-    }
+    // quote deconstructor
+    quote::~quote() { }
     
-    /*
-    * std::cout << the period of the quote:
-    * 
-    * START: YYYY-MM-DD -- (UNIX_TIMESTAMP)
-    * END--: YYYY-MM-DD -- (UNIX_TIMESTAMP)
-    * 
-    */
-    void quote::print_period()
-    {
-        std::cout << "START: " << this->m_date_start[0] << " -- (" << this->m_date_start[1] << ")" << '\n';
-        std::cout << "END--: " << this->m_date_end[0]   << " -- (" << this->m_date_end[1]   << ")" << '\n';
-    }
-    
-    /*
-    * Sets the quote symbol ex: "GME"
-    */
+    // Sets the quote symbol ex: "GME"
     void quote::set_symbol(std::string t_symbol)
     {
         this->m_symbol = t_symbol;
     }
     
-    quote::~quote() { }
+    // Sets the quote period from a start and end date "YYYY-MM-DD"
+    void quote::set_period(std::string t_date_start, std::string t_date_end)
+    {
+        this->m_date_start = t_date_start;
+        this->m_date_end = t_date_end;
+
+    }
+    
+    // Sets the quote period interval: ["1d", "1wk", "1mo"]
+    void quote::set_interval(std::string t_interval)
+    {
+        this->m_interval = t_interval;
+    }
+
+    // Downloads historical stock data for quote
+    void quote::download_quote_data(std::string filename)
+    {
+        CURL *curl;
+        FILE *fp;
+        CURLcode res;
+        curl = curl_easy_init();
+        if (curl)
+        {
+            fp = fopen(filename.c_str(), "wb");
+            curl_easy_setopt(curl, CURLOPT_URL, this->m_url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+            res = curl_easy_perform(curl);
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+            fclose(fp);
+        }
+
+    }
+    
+    // Prints all quote information 
+    void quote::print_quote_info()
+    {
+        std::cout << "Quote Symbol      : " << this->m_symbol << '\n';
+        std::cout << "Quote Date (Start): " << this->m_date_start << ", " << unix_timestamp(this->m_date_start) << '\n';
+        std::cout << "Quote Date (End)  : " << this->m_date_end   << ", " << unix_timestamp(this->m_date_end)   << '\n';
+        std::cout << "Quote Interval    : " << this->m_interval << '\n';
+        std::cout << "Download URL      : " << this->m_url << '\n';
+
+    }
+    
+    // Takes a date in the format YYYY-MM-DD and returns a unix timestamp.
+    std::string quote::unix_timestamp(std::string t_date)
+    {
+        std::istringstream iss(t_date);
+        
+        struct std::tm time = {0,0,0,0,0,0,0,0,0};
+        iss >> std::get_time(&time, "%Y-%m-%d"); 
+        if(iss.fail())
+        {
+            std::cerr  << "ERROR: Cannot parse date (" << t_date <<"); required format YYYY-MM-DD" << std::endl;
+            exit(1);
+        }
+        
+        time.tm_hour = 0;
+        time.tm_min  = 0;
+        time.tm_sec  = 0;
+        std::time_t epoch = std::mktime(&time);
+        return std::to_string(epoch);
+    }
+
+    // Replaces string "{from}" to "{to}"
+    bool quote::string_replace(std::string& str, const std::string from, const std::string to)
+    {
+        size_t start = str.find(from);
+        if(start == std::string::npos)
+        {
+            return false;
+        }
+        str.replace(start, from.length(), to);
+        return true;
+    }
+    
+    // Builds download url from quote information. Requires symbol, start_date, end_date, interval
+    void quote::build_url()
+    {
+        std::string url = this->m_url;
+        string_replace(url, "{symbol}"        , this->m_symbol);
+        string_replace(url, "{date_start}"    , unix_timestamp(this->m_date_start));
+        string_replace(url, "{date_end}"      , unix_timestamp(this->m_date_end));
+        string_replace(url, "{interval}"      , this->m_interval);
+        this->m_url = url;
+    }
+
+    size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+    {
+        size_t written;
+        written = fwrite(ptr, size, nmemb, stream);
+        return written;
+    }
+    
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "include/stockerAPI.hpp"
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
-    stocker::quote Quote;
-    Quote.set_period("2020-01-01", "2020-05-01");
-    Quote.set_interval("1wk");
-    Quote.print_period();
+    stocker::quote Quote("GME", "2020-01-01", "2020-05-01", "1wk");
+    Quote.print_quote_info();
+    Quote.download_quote_data("..\\..\\stock_data\\gme_data.csv");
 
-    return 0;
 }
