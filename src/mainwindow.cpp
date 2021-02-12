20a629435298b53f0915fa5cf0f14f9f1f13b131
DavidM-Fox <david.mac.fox@gmail.com>
2021-02-12 16:29:10 -0600

    Further Edits

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,26 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     // Import ui from QT Designer
     ui->setupUi(this);
-    std::string data_file = "..\\..\\stock_data\\Weekly_GME.csv"; 
-    std::string title_series = "GME"; 
-    std::string title_chart = "Weekly Data";
-    
-    std::string data_file_b = "..\\..\\stock_data\\Monthly_GME.csv"; 
-    std::string title_series_b = "GME"; 
-    std::string title_chart_b = "Monthly Data";
-
-    QCandlestickChart *chart_a = new QCandlestickChart(
-        data_file,
-        title_series,
-        title_chart);
-    
-    QCandlestickChart *chart_b = new QCandlestickChart(
-        data_file_b,
-        title_series_b,
-        title_chart_b);
-
-    ui->gridLayout->addWidget(chart_a->get_ChartView(), 0, 0);
-    ui->gridLayout->addWidget(chart_b->get_ChartView(), 0, 1);
+    ui->stockViewButton->setIcon(QIcon("..\\..\\icons\\icons8-stocks-64.png"));
 }
 
 // MainWindow Deconstructor
